1225add6e50242e51f41d614c953ced5e30ac8c2
restumi <maulanaismael5@gmail.com>
2025-07-24 10:52:57 +0700

    first try node.js

---
+++ api/chat.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export default async function hendler(req, res) {
+    if(req.method !== "POST"){
+        return res.status(405).json({ error : "method not allowed"})
+    }
+
+    const { message } = req.body
+
+    try{
+        const geminiRes = await axios.post(
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyA5ZlyxAVEMdPKjw2-3LEBdOcN0vDx4o_4",
+            {
+                contens : [{ parts : [{ text : message }] }]
+            }
+        );
+
+        const geminiTex = geminiRes.data.candidates[0].content.part[0].text;
+
+        res.status(200).json({ response : geminiTex })
+    } catch (err) {
+        res.status(500).json({ err : "gagal terhubung" })
+    }
+}
